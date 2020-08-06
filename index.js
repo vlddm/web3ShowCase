51481ac0e65f88909ab8e1a5b8dbe25d9a2389a3
vlddm <you@example.com>
2020-08-07 01:47:55 +0300

    Use gasOracle to fetch gas price

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bip39 = require('bip39');
 const bip32 = require('bip32');
 const ethUtil = require('ethereumjs-util')
 
+
 const net = 'ropsten'
 const infuraId = '976948fad74245418d5439def72b9c96'
 const web3 = new Web3(`wss://${net}.infura.io/ws/v3/${infuraId}`)
@@ -37,21 +38,21 @@ async function sendTx(privateKey) {
     const amount = '0.0001' 
     const destAddress = '0x96C32BD226B4A81A81c6159a139c05bd7C7C993d'
     const gasLimit = '21000'
-    const gasPrice = '3' // Gwei
-    const nonce = await web3.eth.getTransactionCount(account.address, 'latest');
-    console.log(`Sending ${amount} ETH from ${account.address} to ${destAddress}, nonce: ${nonce}`)
+    const gasPrice = await web3.eth.getGasPrice()
+    const nonce = await web3.eth.getTransactionCount(account.address, 'latest')
+    console.log(`Sending ${amount} ETH from ${account.address} to ${destAddress}, nonce: ${nonce}, gasPrice: ${web3.utils.fromWei(gasPrice, 'gwei')} Gwei`)
     
     let signedTx = await web3.eth.accounts.signTransaction({
         to: destAddress,
         value: web3.utils.toWei(amount),
         gas: gasLimit,
-        gasPrice: web3.utils.toWei(gasPrice, 'gwei'),
+        gasPrice: gasPrice,
         nonce: nonce
     }, privateKey)
 
     web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-    .on('transactionHash', txHash => console.log(`got transaction hash: ${txHash}`))
-    .on('receipt', receipt => console.log(`Transaction included into block`, receipt))
+    .on('transactionHash', txHash => console.log(`Got transaction hash: ${txHash}`))
+    .on('receipt', receipt => console.log(`Transaction included into block`, receipt.blockNumber))
     .on('error', error=>console.error(error))
 
 }
